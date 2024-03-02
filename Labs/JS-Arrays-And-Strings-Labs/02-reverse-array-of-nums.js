de35f0476269ce54862b89711a8d31f8403a7483
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-03-02 19:13:49 +0200

    add js practice exercises

---
+++ Labs/JS-Arrays-And-Strings-Labs/02-reverse-array-of-nums.js
@@ -0,0 +1,18 @@
+function solve(n, inputArr) {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(inputArr[i]);
+    }
+
+    let output = ""
+    for (let i = arr.length - 1; i >= 0; i--) {
+        output += ` ${arr[i]}`;
+    }
+
+    console.log(output);
+
+}
+
+solve(3, [10, 20, 30, 40, 50]);
+solve(4, [-1, 20, 99, 5]);
+solve(2, [66, 43, 75, 89, 47]);
---
+++ Labs/JS-Arrays-And-Strings-Labs/06-count-string-occurrences.js
@@ -0,0 +1,18 @@
+function solve(string, searchedWord) {
+    let words = string.split(' ');
+    let counter = 0;
+    for (let word of words) {
+        if (word === searchedWord) {
+            counter += 1;
+        }
+    }
+
+    console.log(counter);
+}
+
+solve('This is a word and it also is a sentence',
+'is'
+);
+solve('softuni is great place for learning new programming languages',
+'softuni'
+);
