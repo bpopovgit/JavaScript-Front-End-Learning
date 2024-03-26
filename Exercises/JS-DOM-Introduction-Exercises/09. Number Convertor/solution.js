d680ec5f8e8bc894f55d9a72918ce744e62fe6fe
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-03-26 22:07:29 +0200

    add js practice exercises

--- Exercises/JS-DOM-Introduction-Exercises/09. Number Convertor/solution.js
+++ Exercises/JS-DOM-Introduction-Exercises/09. Number Convertor/solution.js
@@ -1,4 +1,75 @@
 function solve() {
+    const inputNumberElement = document.getElementById('input');
+    const resultElement = document.getElementById('result');
+    // const selectMenuFromElement = document.getElementById('selectMenuFrom');
+    const selectMenuToElement = document.getElementById('selectMenuTo');
+    const convertButtonElement = document.querySelector('button');
 
-    //TODO...
+
+    // Populate select Menu To with options
+
+    // Using innerHTML (NOT RECOMMENDED!)
+    const binaryOptionElement = selectMenuToElement.querySelector('option');
+    binaryOptionElement.value = 'binary';
+    binaryOptionElement.textContent = 'Binary';
+
+    const hexadecimalOptionElement = document.createElement('option');
+    hexadecimalOptionElement.value = 'hexadecimal';
+    hexadecimalOptionElement.textContent = 'Hexadecimal';
+    selectMenuToElement.appendChild(hexadecimalOptionElement);
+
+    const convertors = {
+        binary: convertDecimalToBinary,
+        hexadecimal: convertDecimalToHex,
+
+    };
+
+    convertButtonElement.addEventListener('click', () => {
+        const numberValue = Number(inputNumberElement.value);
+
+        resultElement.value = convertors[selectMenuToElement.value](numberValue)
+    });
+
+    
+
+    function convertDecimalToBinary(number) {
+        const result = [];
+        while (number > 0) {
+            result.push(number % 2)
+            number = Math.trunc(number / 2);
+        }
+
+        return result.reverse().join('');
+    }
+
+    function convertDecimalToHex(number = 10) {
+        const digitMapping = new Map([
+            [0, '0'],
+            [1, '1'],
+            [2, '2'],
+            [3, '3'],
+            [4, '4'],
+            [5, '5'],
+            [6, '6'],
+            [7, '7'],
+            [8, '8'],
+            [9, '9'],
+            [10, 'A'],
+            [11, 'B'],
+            [12, 'C'],
+            [13, 'D'],
+            [14, 'E'],
+            [15, 'F']
+        ])
+
+        const result = [];
+
+        while (number > 0) {
+            result.push(digitMapping.get(number % 16));
+            number = Math.trunc(number / 16);
+        }
+
+        return result.reverse().join('');
+    }
+
 }
