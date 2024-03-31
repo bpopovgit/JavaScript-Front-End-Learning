6b166d522dd6e778a2cd0f208d3941f29777ab7b
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-03-31 22:30:21 +0300

    add js exercises

---
+++ Exercises/JS-DOM-Manipulation-and-Events-Exercises/01. Sections/script.js
@@ -0,0 +1,31 @@
+function create(words) {
+   const contentElement = document.getElementById('content'); 
+
+   const divElements = words
+      .map(word => {
+         const pElement = document.createElement('p');
+         pElement.textContent = word;
+         pElement.style.display = 'none';
+
+         const divElement = document.createElement('div');
+         divElement.appendChild(pElement);
+
+         divElement.addEventListener('click', () => {
+            pElement.style.display = 'block';
+         });
+
+         return divElement;
+      });
+
+      // divElements.forEach(divElement => {
+      //    divElement.addEventListener('click', () => {
+               // const pElement = document.querySelector('p');
+               // pElement.style.display = 'block';
+      //    })
+      // });
+
+   // Append all to DOM
+
+   // contentElement.append(...divElements);
+   divElements.forEach(divElement => contentElement.appendChild(divElement));
+}
